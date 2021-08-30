a8853c62a3be8af33fd5774dfb308cdf7468103f
sabreys <34985378+sabreys@users.noreply.github.com>
2021-08-30 05:01:17 +0300

    Update country_codes.dart

--- lib/src/res/country_codes.dart
+++ lib/src/res/country_codes.dart
@@ -1,7 +1,7 @@
 final List<Map<String, dynamic>> countryCodes = [
   {
     "e164_cc": "999",
-    "iso2_cc": "🏳️",
+    "iso2_cc": "ALL",
     "e164_sc": 0,
     "geographic": true,
     "level": 1,
